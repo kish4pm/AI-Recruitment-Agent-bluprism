fc92e8f5c04eb791b3318abbd0c1b49200e998a7
Ayoub <you@example.com>
2025-07-02 03:21:18 +0200

    update voice

---
+++ app/provider.jsx
@@ -0,0 +1,53 @@
+"use client"
+import { UserDetailContext } from '@/context/UserDetailContext';
+import {supabase} from '@/services/supabaseClient'
+import React, {useState, useEffect, useContext} from 'react'
+
+
+function Provider({ children }) {
+
+    const [user, setUser] = useState();
+    useEffect(()=>{
+        CreateNewUser();
+    },[])
+
+    const CreateNewUser = () =>{
+
+        supabase.auth.getUser().then(async ({data: {user}})=>{
+            //check if user exist
+            let {data : users, error} = await supabase
+            .from('users')
+            .select('*').eq('email', user?.email);
+
+        console.log(users)
+
+        if(users?.length === 0){      
+            //create new user
+            const {data, error} = await supabase.from('users')
+            .insert([
+                {
+                name:user?.user_metadata?.name,
+                email:user?.email,
+                picture:user?.user_metadata?.picture
+                }
+        ])
+        console.log(data);
+        setUser(data);
+        return;
+        }
+        setUser(users[0]);
+        })
+    }
+    return (
+        <UserDetailContext.Provider value={{user, setUser }}>
+            <div>{children}</div>
+        </UserDetailContext.Provider>
+    );
+}
+
+export default Provider;
+
+export const useUser=()=>{
+    const context=useContext(UserDetailContext);
+    return context;
+}
